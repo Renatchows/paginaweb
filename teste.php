374713e86bfa95652eaa617f7f2131dd2e574082
Renatchows <34916928+Renatchows@users.noreply.github.com>
2019-11-30 19:45:35 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,12 @@
+<form method=POST action="mailto:renato.cavalcante.silva@gmail.com" enctype="text/plain">
+
+Nome<br>
+<input type="text" name="nome"><br>
+Item<br>
+<input type="text" name="item"><br>
+Quantidade<br>
+<input type="tel" name="phonenumber" min="6" max="15"><br>
+
+<input type="submit" value="Enviar">
+
+</form>
